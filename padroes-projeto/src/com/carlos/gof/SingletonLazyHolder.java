544dd77a7da4016de8c914ed9eeec4a2e93330d8
carlos4565mana <carloscal61@gmail.com>
2022-07-13 14:03:59 -0300

    Padrao Singleton

---
+++ padroes-projeto/src/com/carlos/gof/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package com.carlos.gof;
+/**
+ * Lazy Holder
+ *
+ *
+ */
+public class SingletonLazyHolder {
+	
+	private static class InstanceHolder {
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+	}
+	
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return InstanceHolder.instancia;
+	}
+
+}
