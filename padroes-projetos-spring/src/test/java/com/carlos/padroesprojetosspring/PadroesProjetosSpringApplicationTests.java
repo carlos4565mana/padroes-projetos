e9ff2e0b1d986c8c75ca6df34b917157496fb42c
carlos4565mana <carloscal61@gmail.com>
2022-07-13 15:34:20 -0300

    Criado o Projeto Spring

---
+++ padroes-projetos-spring/src/test/java/com/carlos/padroesprojetosspring/PadroesProjetosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.padroesprojetosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
